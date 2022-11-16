9b97d70aabb6b824352362655ac4631adb176342
SWillames <sergiowoc@gmail.com>
2022-11-16 02:24:19 -0300

    add the associations.

--- backend/app/models/artist.rb
+++ backend/app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ApplicationRecord
-  belongs_to :user
+  has_many :records, dependent: :destroy
+
+  validates_presence_of :name
+
+
 end
--- backend/app/models/record.rb
+++ backend/app/models/record.rb
@@ -1,4 +1,5 @@
 class Record < ApplicationRecord
-  belongs_to :artist
   belongs_to :user
+
+  validates_presence_of :title, :year
 end
